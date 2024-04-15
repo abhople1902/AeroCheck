1ae1595482e3aa80a1a72b0991ec9cdfa8fd3294
Ayush Bhople <77747115+abhople1902@users.noreply.github.com>
2024-04-16 02:38:42 +0530

    Updated AeroCheck to work with HangarBay

--- AeroCheck/ViewController.swift
+++ AeroCheck/ViewController.swift
@@ -33,10 +33,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             let fileContent = imageData?.base64EncodedString()
             let postData = fileContent!.data(using: .utf8)
             
-            var request = URLRequest(url: URL(string: "https://detect.roboflow.com/aircraft-defects-lm9e3/3?api_key=_&name=YOUR_IMAGE.jpg")!,timeoutInterval: Double.infinity)
+            
+            var request = URLRequest(url: URL(string: "https://detect.roboflow.com/innovation-hangar-v2/1?api_key=V5UUWMksYOC3S27kgKvW&name=YOUR_IMAGE.jpg")!,timeoutInterval: Double.infinity)
             request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
             request.httpMethod = "POST"
             request.httpBody = postData
+            
             
             
             URLSession.shared.dataTask(with: request, completionHandler: { data, response, error in
@@ -74,6 +76,47 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         present(imagePicker, animated: true, completion: nil)
     }
+    
+    @IBAction func buttonAPIPressed(_ sender: UIButton) {
+        let backendURLString = "http://192.168.1.7:3000/repairs/newfault"
+            guard let backendURL = URL(string: backendURLString) else {
+                print("Invalid backend URL")
+                return
+            }
+
+            // Prepare data to send in the request body
+            let labelText = faultClass.text ?? ""
+            let requestData: [String: Any] = ["name": labelText]
+            print(requestData)
+            
+            do {
+                // Serialize the data into JSON
+                let jsonData = try JSONSerialization.data(withJSONObject: requestData, options: [])
+
+                // Create a URLRequest with the backend URL
+                var request = URLRequest(url: backendURL)
+                request.httpMethod = "POST"
+                request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+                request.httpBody = jsonData
+
+                // Send the request
+                URLSession.shared.dataTask(with: request) { data, response, error in
+                    if let error = error {
+                        print("Error: \(error)")
+                        return
+                    }
+
+                    // Handle the response
+                    if let httpResponse = response as? HTTPURLResponse {
+                        print("Status code: \(httpResponse.statusCode)")
+                        // Process the response data if needed
+                    }
+                }.resume()
+            } catch {
+                print("Error serializing JSON: \(error)")
+            }
+    }
+    
     
 }
 
